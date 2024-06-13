c65e6184f246d776202657d381f1fb6cd82e5029
mohtashim21 <mohteshim3276@gmail.com>
2024-06-13 13:25:49 +0530

    code for contact model/schema

---
+++ src/models/contact.model.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Contact = mongoose.model("Contact", contactSchema);
