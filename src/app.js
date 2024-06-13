47a56f4ac3f98a975294da62995050713363c18a
mohtashim21 <mohteshim3276@gmail.com>
2024-06-13 13:29:43 +0530

    app.js code for api routes, middlewares

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import userRoutes from "./routes/user.routes.js";
+import contactRoutes from "./routes/contact.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use("/auth/user", userRoutes);
+app.use("/api/contact", contactRoutes);
+
+export { app };
