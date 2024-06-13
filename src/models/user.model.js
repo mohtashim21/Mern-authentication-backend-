a82063ab087dcc48e193c2a13b3e257c0f753da8
mohtashim21 <mohteshim3276@gmail.com>
2024-06-13 13:24:52 +0530

    code for user model/schema

---
+++ src/models/user.model.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
