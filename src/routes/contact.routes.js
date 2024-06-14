6e1a3164f36f5651d877cbf0e57b22bb5e5b7619
mohtashim21 <mohteshim3276@gmail.com>
2024-06-14 21:15:38 +0530

    update contact routes

--- src/routes/contact.routes.js
+++ src/routes/contact.routes.js
@@ -6,13 +6,14 @@ import {
   deleteContact,
   deleteAllContact,
 } from "../controllers/contact.controller.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 const router = Router();
 
-router.get("/", getAllContacts);
-router.post("/", addContact);
-router.delete("/:id", deleteContact);
-router.delete("/", deleteAllContact);
-router.put("/:id", updateContact);
+router.get("/", authMiddleware, getAllContacts);
+router.post("/", authMiddleware, addContact);
+router.delete("/:id", authMiddleware, deleteContact);
+router.delete("/", authMiddleware, deleteAllContact);
+router.put("/:id", authMiddleware, updateContact);
 
 export default router;
